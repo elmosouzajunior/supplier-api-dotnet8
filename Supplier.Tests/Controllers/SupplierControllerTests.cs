905cba2627a907ac0aebffce6d1f8aaf52943083
Elmo Souza <elmo.souza@external.liveresia.com>
2025-11-04 17:43:35 -0300

    Testes unitários implementados

---
+++ Supplier.Tests/Controllers/SupplierControllerTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supplier.Tests.Controllers
+{
+    internal class SupplierControllerTests
+    {
+    }
+}
